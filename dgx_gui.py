3e82053d4d94ee4a5ab45962864b6cfd400e6def
Jim Maciejewski <jim.maciejewski@amxaustralia.com.au>
2014-10-17 16:22:54 +1000

    resized the main window

--- dgx_gui.py
+++ dgx_gui.py
@@ -17,7 +17,7 @@ import wx.xrc
 class DGXFrame ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"DGX Syslog monitor", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"DGX Syslog monitor", pos = wx.DefaultPosition, size = wx.Size( 900,450 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
